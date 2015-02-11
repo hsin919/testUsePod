a326f0bde34e8cdb7b40cc524e28141922fad1e3
hsin <hsin919@gmail.com>
2015-02-11 16:12:10 +0800

    Initial Commit

---
+++ testUsePod/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testUsePod
+//
+//  Created by Nathan on 2015/2/11.
+//  Copyright (c) 2015年 Stupid Pencil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testUsePod/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testUsePod
+//
+//  Created by Nathan on 2015/2/11.
+//  Copyright (c) 2015年 Stupid Pencil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
